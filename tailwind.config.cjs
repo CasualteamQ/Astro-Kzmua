1ea4d37a1d91458da81af9396a4ef401134f491c
CasualteamQ <136259661+CasualteamQ@users.noreply.github.com>
2023-09-14 11:17:28 -0300

    Lookanfeel

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,14 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		fontFamily:{
+			'american':['american'],
+			'sfthin':['sfthin'],
+			'montserrat':['montserrat'],
+			'Sunday':['Sunday'],
+
+		}
+		
 	},
 	plugins: [],
 }
